0c1df4b33adf0e0138f07d89eb9d4a5970eb76ce
lohith <sunnapulohithkumar@gmail.com>
2023-06-20 12:51:32 +0530

    modified

--- mul.sh
+++ mul.sh
@@ -1,6 +1,13 @@
+
 #!/bin/bash
 #the above line will tell that to use /bin/bash shell to execute below
 whoami
 pwd
 echo "hi there"
 #end
+wdihfewyfwe
+lcfkwifhwif
+fcnwoifcjwfcj
+2ehfujdfh
+kwjbvjwfujw
+weoifwif
